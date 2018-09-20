6e84549df158bae4b5675c2514eb17c603565616
nithinganji <nithinreddy.ganji@rsrit.com>
2018-09-19 22:57:14 -0400

    added formvalidator

--- src/main/java/com/nithin/jenkins/DynatraceBuilder.java
+++ src/main/java/com/nithin/jenkins/DynatraceBuilder.java
@@ -3,19 +3,28 @@
  */
 package com.nithin.jenkins;
 
+import java.io.IOException;
+
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
 
 import hudson.Extension;
+import hudson.FilePath;
+import hudson.Launcher;
 import hudson.model.AbstractProject;
 import hudson.model.FreeStyleProject;
+import hudson.model.Run;
+import hudson.model.TaskListener;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Builder;
+import hudson.util.FormValidation;
+import jenkins.tasks.SimpleBuildStep;
 
 /**
  * @author Nithin Reddy Ganji
  *
  */
-public class DynatraceBuilder extends Builder {
+public class DynatraceBuilder extends Builder implements SimpleBuildStep {
 
 	private final String environmentId;
 
@@ -49,12 +58,12 @@ public class DynatraceBuilder extends Builder {
 		return passToken;
 	}
 
-	@Override
+	/*@Override
 	public boolean perform(hudson.model.Build<?, ?> build, hudson.Launcher launcher,
 			hudson.model.BuildListener listener) throws InterruptedException, java.io.IOException {
 		System.out.println(environmentId + " " + apiToken + " " + passToken);
 		return true;
-	};
+	};*/
 
 	@Extension
 	public static final class DynatracePluginBuildDescriptor extends BuildStepDescriptor<Builder> {
@@ -70,6 +79,19 @@ public class DynatraceBuilder extends Builder {
 			return "Configure Dynatarce";
 		}
 
+		public FormValidation doTestConnection(@QueryParameter("environmentId") String environmentId,
+				@QueryParameter("apiToken") String apiToken) {
+			System.out.println("Passed envId is " + environmentId + " and api token is " + apiToken);
+			return FormValidation.ok("Test connection successful");
+		}
+
+	}
+
+	@Override
+	public void perform(Run<?, ?> arg0, FilePath arg1, Launcher arg2, TaskListener arg3)
+			throws InterruptedException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println(environmentId + " " + apiToken + " " + passToken);
 	}
 
 }
